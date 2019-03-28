748c72c3edabafcc8575697f3de435ab5737be40
rashikasinghh <rashika.singhh@hotmail.com>
2019-03-28 22:38:07 +0530

    forms done

--- src/app/orders/order.component.ts
+++ src/app/orders/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
 templateUrl: './order.component.html'
@@ -6,4 +7,16 @@ templateUrl: './order.component.html'
 
 export class OrderComponent {
     title: 'Order List ';
+
+orderForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    contact: new FormControl(''),
+    email: new FormControl('', [Validators.required]),
+    address: new FormControl('')
+});
+
+Submit() {
+    const form = this.orderForm.value;
+    console.log(form);
+}
 }
